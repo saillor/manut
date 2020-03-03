ff24e1044b1861b11eb51b0dc7e2576e403adec1
saillor <43767814+saillor@users.noreply.github.com>
2020-03-04 05:37:51 +0700

    Update gojek7.php

--- gojek7.php
+++ gojek7.php
@@ -39,7 +39,7 @@ if ($register == false)
 		$h=fopen("newgojek.txt","a");
 		fwrite($h,json_encode(array('token' => $verif, 'voc' => 'gofood gak ada'))."\n");
 		fclose($h); 
-                echo "\e[!] Trying to redeem Reff : G-2BC8WVL !\n";
+                echo "\e[!] Trying to redeem Reff : G-6W9WNJ4 !\n";
                 sleep(3);
             $claim = reff($verif);
             if ($claim == false){
